8ee074923c157bd2c985af101029270a2ba6fd38
Nikita Sheremet <nikita.sheremet253@gmail.com>
2021-03-28 15:40:42 -0400

    Add loogic to be able to update user details

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
+const e = require("express")
 const express = require("express")
 const { MongoClient } = require("mongodb")
+const bodyParser = require("body-parser")
+
 require("dotenv").config()
 const app = express()
 const port = 3000
 
-app.get("/", (req, res) => {
-    res.send("Hello World!")
-})
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+const uri = `mongodb+srv://closetlyAdmin:${process.env.DB_PASS}@cluster0.vt6bu.mongodb.net/closetly?retryWrites=true&w=majority&useUnifiedTopology=true`
+
 app.post("/createUser", async (req, res) => {
-    const { e: email, p: password } = req.query
+    const { email, password } = req.body
     if (email && password) {
-        const uri = `mongodb+srv://closetlyAdmin:${process.env.DB_PASS}@cluster0.vt6bu.mongodb.net/closetly?retryWrites=true&w=majority&useUnifiedTopology=true`
         const client = new MongoClient(uri)
         try {
             await client.connect()
@@ -20,16 +24,14 @@ app.post("/createUser", async (req, res) => {
             const checkIfUserExists = await users.findOne(
                 checkIfUserExistsQuery
             )
-            console.log("CHECK IF USER EXISTS", checkIfUserExists)
             if (!checkIfUserExists) {
                 let userDoc = {
-                    email: email,
+                    email,
                     pass: password,
                 }
                 await users.insertOne(userDoc)
                 const query = { email }
                 const user = await users.findOne(query)
-                console.log(user)
                 res.status(200).send(`User created ${JSON.stringify(user)}`)
             } else {
                 res.status(400).send(`That email already exists`)
@@ -43,6 +45,45 @@ app.post("/createUser", async (req, res) => {
     } else {
         res.status(400).send("Does not contain proper params")
     }
+})
+
+app.post("/updateUser", async (req, res) => {
+    const { currentEmail, updateObject } = req.body
+    if (currentEmail && updateObject) {
+        const client = new MongoClient(uri)
+        try {
+            await client.connect()
+            const database = client.db("closetly")
+            const users = database.collection("users")
+            const checkIfUserExistsQuery = { email: currentEmail }
+            const checkIfUserExists = await users.findOne(
+                checkIfUserExistsQuery
+            )
+            if (checkIfUserExists) {
+                let filter = { _id: checkIfUserExists._id }
+                let updateDocument = {
+                    $set: updateObject,
+                }
+                await users.updateOne(filter, updateDocument)
+                const query = { _id: checkIfUserExists._id }
+                const user = await users.findOne(query)
+                res.status(200).send(`User updated ${JSON.stringify(user)}`)
+            } else {
+                res.status(400).send(`Invalid email`)
+            }
+        } catch (err) {
+            console.dir(err)
+            res.status(400).send(`Could not update user ${JSON.stringify(err)}`)
+        } finally {
+            await client.close()
+        }
+    } else {
+        res.status(400).send("Does not contain proper params")
+    }
+})
+
+app.get("/", (req, res) => {
+    res.send("Hello World!")
 })
 
 app.listen(port, () => {
