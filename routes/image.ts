95542d9a2c552a2f76bcff8f8e4ee75325331493
Nikita Sheremet <nikita.sheremet253@gmail.com>
2022-07-28 21:38:51 -0400

    update upload image route

--- routes/image.ts
+++ routes/image.ts
@@ -2,9 +2,16 @@ require("dotenv").config()
 import * as express from "express"
 import { MongoClient, ObjectId } from "mongodb"
 import authenticateRequest from "../middleware/authenticateRequest"
+import * as multer from "multer"
+import { v2 as cloudinary } from "cloudinary"
+
+// Return "https" URLs by setting secure: true
+cloudinary.config({
+    secure: true,
+})
 
 const imageRouter = express.Router()
-
+const upload = multer({ dest: ".temp" })
 const uri = `mongodb+srv://closetlyAdmin:${process.env.DB_PASS}@cluster0.vt6bu.mongodb.net/closetly?retryWrites=true&w=majority&useUnifiedTopology=true`
 
 imageRouter.get("/showPictures", authenticateRequest, async (req, res) => {
@@ -65,29 +72,54 @@ imageRouter.post("/updateImage", async (req, res) => {
     }
 })
 
-imageRouter.post("/saveImage", async (req, res) => {
-    const { description, title, name, url, tags } = req.body
-    const client = new MongoClient(uri)
-    try {
-        await client.connect()
-        const database = client.db("closetly")
-        const images = database.collection("images")
-        let imageDoc = {
-            description,
-            title,
-            name,
-            url,
-            user: res.locals.userId,
-            tags,
+imageRouter.post(
+    "/saveImage",
+    authenticateRequest,
+    upload.single("closet-image"),
+    async (req, res) => {
+        const { description, title, tags } = req.body
+
+        try {
+            const cloudinaryImageUploadOptions = {
+                use_filename: true,
+                unique_filename: false,
+                overwrite: true,
+            }
+            // Upload the image
+            const { url, public_id } = await cloudinary.uploader.upload(
+                req.file.path,
+                cloudinaryImageUploadOptions
+            )
+            const client = new MongoClient(uri)
+            try {
+                // Save image to db
+                await client.connect()
+                const database = client.db("closetly")
+                const images = database.collection("images")
+                let imageDocStructure = {
+                    description,
+                    title,
+                    name: public_id,
+                    url,
+                    user: res.locals.userId,
+                    tags: JSON.parse(tags),
+                }
+                await images.insertOne(imageDocStructure)
+
+                res.status(200).send({ ...imageDocStructure })
+            } catch (err) {
+                const errorMessage = `Unable to save file to db. Error is: ${err}`
+                console.log(errorMessage)
+                res.status(500).send(errorMessage)
+            } finally {
+                await client.close()
+            }
+        } catch (err) {
+            const errorMessage = `unable to upload image, fieldname: ${req.file.fieldname}, originalname: ${req.file.originalname}. Error is: ${err}`
+            console.log(errorMessage)
+            res.status(500).send(errorMessage)
         }
-        await images.insertOne(imageDoc)
-        res.status(200).send(`Image Saved`)
-    } catch (err) {
-        console.dir(err)
-        res.status(400).send(`Could not save image  ${err}`)
-    } finally {
-        await client.close()
     }
-})
+)
 
 export default imageRouter
